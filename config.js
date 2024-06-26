889aabe3d048790ba2ebd48e247f2846f9bc6d06
Truck-kun1816 <ngaphasyuhlo30@gmail.com>
2024-06-27 00:10:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,11 +7,11 @@ global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
 global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
-global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
-global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
+global.ownername = "Truck Kun123";
+global.owner = ['919362599250'];
+global.premium = ['919362599250'];
+global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢 Truck kun123";
+global.author = "Ngapha";
 global.sessionName = 'session';
 global.prefa = [".", "!", "/", "?"];
 global.typemenu = "v1"
@@ -32,10 +32,10 @@ global.mess = {
     banned: '`You are *Banned* fron using commands!`',
     bangc: '`This Group is *Banned* from using Commands!`',
     success: '`✓ Success`',
-    admin: '`This feature is only for group admins`',
+    admin: '`He feature hi group admin tan chauh a hman thei`',
     botAdmin: '`I am not an admin!`',
-    owner: '`You are not my owner`',
-    group: '`You can use this command only in groups ❌`',
+    owner: '`A neitu i nilo`',
+    group: '`He command hi group ah chiah i hmang thei❌`',
     private: '`Feature is used only for private chats!`',
     bot: '`Bot number user special features`',
     wait: '*Processing Your request*',
